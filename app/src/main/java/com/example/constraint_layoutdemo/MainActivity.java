3fd3936ac25ed4035012ae2180bc4f16d9ad64c5
mayur07 <mayurmorem4180@gmail.com>
2024-03-13 12:27:56 +0530

    Final Commit

---
+++ app/src/main/java/com/example/constraint_layoutdemo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.constraint_layoutdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"From Submitted..",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
